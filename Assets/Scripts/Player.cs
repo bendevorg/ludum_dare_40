fa26f424f42fe61b195f0cb796deed1c729f27d4
GuiMac <guibasconti@gmail.com>
2017-12-02 07:35:56 -0200

    Die on wall dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Controller2D))]
-public class Player : MonoBehaviour {
+public class Player : LivingEntity {
 
 	Controller2D controller;
 	BoxCollider2D collider;
@@ -72,6 +72,8 @@ public class Player : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.collider.tag == "Enemy" && playerInfo.attack){
 			other.collider.GetComponent<LivingEntity>().TakeDamage(5);
+		} else if (other.collider.tag == "Wall" && playerInfo.onDash){
+			transform.GetComponent<LivingEntity>().TakeDamage(99999);
 		}
 	}
 
