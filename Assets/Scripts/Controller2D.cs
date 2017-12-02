2b1967def99ffdbcbe8f34d569762c211fffc84e
GuiMac <guibasconti@gmail.com>
2017-12-02 03:33:56 -0200

    Player moves

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Controller2D : MonoBehaviour {
 
-	public void Move(Vector3 input){
-		return;
+	Rigidbody2D rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(Vector2 input){
+		rb.MovePosition(rb.position + input);
 	}
 	
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour {
 
+	Controller2D controller;
+
+	Vector2 input;
+	public float speed = 2f;
+
 	// Use this for initialization
 	void Start () {
-		
+		controller = GetComponent<Controller2D>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+	}
+
+	void FixedUpdate(){
+		controller.Move(input * Time.deltaTime * speed);
 	}
 }
