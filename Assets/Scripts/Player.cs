33e6607fdcddd6ae2259b3681aaf090fc77f15b3
GuiMac <guibasconti@gmail.com>
2017-12-02 03:54:45 -0200

    Movement and smooth movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour {
 	Controller2D controller;
 
 	Vector2 input;
-	public float speed = 2f;
+	Vector3 velocity;
+	float velocityXSmoothing;
+	float velocityYSmoothing;
+	float accelerationTime = .1f;
+	public float moveSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +25,10 @@ public class Player : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		controller.Move(input * Time.deltaTime * speed);
+		float targetVelocityX = input.x * moveSpeed;
+		float targetVelocityY = input.y * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTime);
+		velocity.y = Mathf.SmoothDamp(velocity.y, targetVelocityY, ref velocityYSmoothing, accelerationTime);
+		controller.Move(velocity * Time.deltaTime);
 	}
 }
