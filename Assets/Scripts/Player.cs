063ac1de096b945572ea3277a1084de81d86280f
GuiMac <guibasconti@gmail.com>
2017-12-02 05:50:30 -0200

    Nivaldinho

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,51 +6,81 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	Controller2D controller;
+	PlayerInfo playerInfo;
 
 	Vector2 input;
-	Vector3 velocity;
 	float velocityXSmoothing;
 	float velocityYSmoothing;
 	float accelerationTime = .1f;
 	public float moveSpeed = 2f;
 
-	public float dashSpeedMultiplier = 4f;
+	public float dashSpeedMultiplier = 40f;
 	public float dashTime = .25f;
-	bool onDash = false;
 	float dashTimeRemaining;
 
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<Controller2D>();	
+		playerInfo = new PlayerInfo(false, true, moveSpeed, accelerationTime, Vector3.zero);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!onDash){
+		if (playerInfo.inputEnabled){
 			input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 			input = input.normalized;
-		} else if(dashTimeRemaining >= dashTime) {
-			onDash = false;
-			moveSpeed /= dashSpeedMultiplier;
-		} else {
-			dashTimeRemaining += Time.deltaTime;
+		} else if(playerInfo.onDash) {
+			Dash();
 		}
 
 		if (Input.GetKeyDown(KeyCode.Space)){
-			if (!onDash){
-				onDash = true;
-				moveSpeed *= dashSpeedMultiplier;
-				dashTimeRemaining = 0f;
+			if (!playerInfo.onDash){
+				Dash();
 			}
 		}
 	}
 
 	void FixedUpdate(){
-		float targetVelocityX = input.x * moveSpeed;
-		float targetVelocityY = input.y * moveSpeed;
-		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTime);
-		velocity.y = Mathf.SmoothDamp(velocity.y, targetVelocityY, ref velocityYSmoothing, accelerationTime);
-		controller.Move(velocity * Time.deltaTime);
+		float targetVelocityX = input.x * playerInfo.moveSpeed;
+		float targetVelocityY = input.y * playerInfo.moveSpeed;
+		playerInfo.velocity.x = Mathf.SmoothDamp(playerInfo.velocity.x, targetVelocityX, ref velocityXSmoothing, playerInfo.accelerationTime);
+		playerInfo.velocity.y = Mathf.SmoothDamp(playerInfo.velocity.y, targetVelocityY, ref velocityYSmoothing, playerInfo.accelerationTime);
+		controller.Move(playerInfo.velocity * Time.deltaTime);
 	}
 
+	void Dash(){
+		if (!playerInfo.onDash){
+			playerInfo.onDash = true;
+			playerInfo.inputEnabled = false;
+			playerInfo.moveSpeed *= dashSpeedMultiplier;
+			Debug.Log(playerInfo.moveSpeed);
+			dashTimeRemaining = 0f;
+			playerInfo.accelerationTime = 0f;
+		} else if (dashTimeRemaining < dashTime){
+			dashTimeRemaining += Time.deltaTime;
+			Debug.Log("a");
+		} else {
+			Debug.Log("b");
+			playerInfo.onDash = false;
+			playerInfo.inputEnabled = true;
+			playerInfo.moveSpeed /= dashSpeedMultiplier;
+			playerInfo.accelerationTime = accelerationTime;
+		}
+	}
+
+	struct PlayerInfo {
+		public bool onDash;
+		public bool inputEnabled;
+		public float moveSpeed;
+		public float accelerationTime;
+		public Vector3 velocity;
+
+		public PlayerInfo(bool _onDash, bool _inputEnabled, float _moveSpeed, float _accelerationTime, Vector3 _velocity){
+			onDash = _onDash;
+			inputEnabled = _inputEnabled;
+			moveSpeed = _moveSpeed;
+			accelerationTime = _accelerationTime;
+			velocity = _velocity;
+		}
+	}
 }
